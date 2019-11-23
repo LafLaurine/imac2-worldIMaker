bd08edd616ba22a145759867dea10de26c09e8de
flora mallet <iciflaurore33@orange.fr>
2019-11-23 16:30:16 +0100

    Cube class
    Cube class : attributes, getters, setters

--- src/include/Cube.hpp
+++ src/include/Cube.hpp
@@ -3,8 +3,30 @@
 
 class Cube
 {
+	protected:
+		double m_height;
+		double m_width;
+		double m_length;
+		bool empty;
+
 	public:
-	void display(glm::vec3 position,GLuint tex_id);
+		// Builder
+		Cube() = default;
+
+		// Destroyer
+		~Cube();
+
+		// Getter
+		inline double getHeight()const{ return m_height; };
+		inline double getWidth()const{ return m_width; };
+		inline double getLength()const{ return m_length; };
+
+		// Setter
+		void setHeight(Cube &cubeObj);
+		void setWidth(Cube &cubeObj);
+		void setLength(Cube &cubeObj);
+
+		void display(glm::vec3 position,GLuint tex_id);
 };
 
 #endif // WORL_IMAKER_CUBE_HPP
