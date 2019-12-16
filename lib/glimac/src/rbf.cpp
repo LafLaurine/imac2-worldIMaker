a1718c9cab62b37ddc916ccad860b6535dd08581
flora mallet <iciflaurore33@orange.fr>
2019-12-16 10:52:06 +0100

    rbf

--- lib/glimac/src/rbf.cpp
+++ lib/glimac/src/rbf.cpp
@@ -8,7 +8,20 @@ namespace glimac{
             T d = vectors_distance(v1, v2);
             return exp(-epsilon*d*d); 
         }
+        else if(type == FunctionType::ThinPlateSpline) {
+        	T d = vectors_distance(v1, v2);
+            return pow(vectors_distance, 2)*log(vectors_distance);
+        }
         else if(type == FunctionType::InverseQuadratic) {
+        	T d = vectors_distance(v1, v2);
+        	return pow(1+pow((epsilon*vectors_distance),2),-1);
+        }
+        else if(type == FunctionType::BiharmonicSpline) {
+        	return vectors_distance(v1, v2);
+        }
+        else if(type == FunctionType::Multiquadric) {
+        	T d = vectors_distance(v1, v2);
+        	return sqrt(1+(epsilon*pow(vectors_distance,2)));  //sqrt(1 + (epsiolon * r^2))
         }
     }
 
